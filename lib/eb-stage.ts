9a434d0d997fdfe33c21a34318bb749833525105
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2023-06-13 15:24:23 -0700

    Reverted app name back to elastic_beanstalk_practice_app

--- lib/eb-stage.ts
+++ lib/eb-stage.ts
@@ -6,15 +6,16 @@ import { EBEnvProps, EBApplnStack } from './eb-appln-stack';
  * Deployable unit of web service app
  */
 export class CdkEBStage extends Stage {
-    constructor(scope: Construct, id: string, props?: EBEnvProps) {
-        super(scope, id, props);
+      
+  constructor(scope: Construct, id: string, props?: EBEnvProps) {
+    super(scope, id, props);
 
-        const service = new EBApplnStack(this, 'WebService', {
-            minSize : props?.minSize,
-            maxSize : props?.maxSize,
-            instanceTypes : props?.instanceTypes,
-            envName : props?.envName
-        });
+    const service = new EBApplnStack(this, 'WebService', {
+        minSize : props?.minSize, 
+        maxSize : props?.maxSize,
+        instanceTypes : props?.instanceTypes,
+        envName : props?.envName
+    } );
 
-    }
+  }
 }
