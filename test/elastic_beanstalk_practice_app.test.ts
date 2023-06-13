aefb682a3ce673820fd5107a1c28f56383b50e18
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2023-06-13 15:15:57 -0700

    Reverted app name back to elastic_beanstalk_practice_app

--- test/elastic_beanstalk_practice_app.test.ts
+++ test/elastic_beanstalk_practice_app.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as ElasticBeanstalkPracticeApp from '../lib/cdk-pipeline-eb-demo-stack';
+// import * as ElasticBeanstalkPracticeApp from '../lib/elastic_beanstalk_practice_app-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-pipeline-eb-demo-stack.ts
+// example resource in lib/elastic_beanstalk_practice_app-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
